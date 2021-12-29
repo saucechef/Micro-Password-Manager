f38f56d50eb92e1be0d76c3e40fe38cc374882a4
saucechef <tom.staab@toms-spot.de>
2021-12-29 14:46:10 +0100

    Added functionality to access services.txt and delete password from within the application.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,61 +13,22 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->comboService, SIGNAL(currentTextChanged(QString)), this, SLOT(textChanged()));
     connect(ui->comboService, SIGNAL(currentIndexChanged(QString)), this, SLOT(indexChanged()));
     connect(ui->buttonCopyUsername, SIGNAL(clicked()), this, SLOT(usernameToClipboard()));
+    connect(ui->actionSavePassword, SIGNAL(triggered()), this, SLOT(savePassword()));
+    connect(ui->actionEditServices, SIGNAL(triggered()), this, SLOT(editServices()));
+    connect(ui->actionReloadServices, SIGNAL(triggered()), this, SLOT(loadServices()));
+    connect(ui->actionDeletePassword, SIGNAL(triggered()), this, SLOT(deletePassword()));
     connect(&watcher, SIGNAL(finished()), this, SLOT(passwordToClipboard()));
 
     // Get system clipboard
     clipboard = QApplication::clipboard();
 
-    // Load file of services
-    servicesFile = new QFile("services.txt");
-    if (servicesFile->open(QIODevice::ReadOnly | QIODevice::Text)) {
-        QString servicesString = QString::fromUtf8(servicesFile->readAll());
-        QStringList servicesRows = servicesString.split('\n', QString::SplitBehavior::SkipEmptyParts);
-
-        foreach (QString row, servicesRows) { // Split services and usernames into map
-            QStringList buffer = row.split('/', QString::SplitBehavior::SkipEmptyParts);
-            if (buffer.length() == 1) {
-                usernameMap.insert(buffer.at(0), "n/a");
-            } else if (buffer.length() == 2) {
-                usernameMap.insert(buffer.at(0), buffer.at(1));
-            } else if (buffer.length() == 3) {
-                usernameMap.insert(buffer.at(0), buffer.at(1));
-                charsetMap.insert(buffer.at(0), buffer.at(2));
-            }
-        }
-
-        QStringList services = QStringList(usernameMap.keys());
-        for(int k = 0; k < (services.size()/2); k++) services.swap(k, services.size()-(1+k));
-
-        ui->comboService->insertItems(0, services);
-        ui->comboService->setCurrentText("");
-        ui->comboService->setCurrentIndex(-1);
-        servicesFile->close();
-    }
+    // Load file of services and password
+    loadServices();
+    loadPassword();
 
     // Set password line to hide input
     ui->linePassword->setEchoMode(QLineEdit::Password);
-    textChanged();
-
-    // Load password
-    passwordFile = new QFile("password");
-    if (passwordFile->open(QIODevice::ReadOnly)) {
-        QByteArray* key = crypt_backend::getLocalKey();
-        QByteArray* passwordBytes = crypt_backend::decrypt(passwordFile->readAll(), *key);
-        QString passwordPlain = QString::fromUtf8(*passwordBytes);
-        if (passwordPlain.contains("PW:")) { // Check if password is valid
-            passwordPlain.remove(0, 3);
-            ui->linePassword->setText(passwordPlain);
-            ui->boxSavePassword->setText("Overwrite password?");
-            status("Password loaded", "#ddffdd");
-        } else {
-            status("Password decryption failed", "#ffdddd");
-        }
-        passwordFile->close();
-        delete key;
-        delete passwordBytes;
-    }
-
+    textChanged(); 
 }
 
 MainWindow::~MainWindow() {
@@ -96,27 +57,6 @@ void MainWindow::generate() {
     QString charset = charsetMap.value(ui->comboService->currentText(), crypt_backend::defaultCharset);
     future = QtConcurrent::run(crypt_backend::generator, ui->linePassword->text(), ui->comboService->currentText().toLower(), charset);
     watcher.setFuture(future);
-
-    // Save master password
-    if (ui->boxSavePassword->isChecked() && passwordFile->open(QIODevice::WriteOnly)) {
-
-        // Obtain key and encrypt password
-        QByteArray* key = crypt_backend::getLocalKey();
-        QByteArray passwordBytes = ("PW:" + ui->linePassword->text()).toUtf8();
-        QByteArray* encrypted = crypt_backend::encrypt(passwordBytes, *key);
-
-        // Save to file
-        passwordFile->write(*encrypted);
-        passwordFile->close();
-
-        // Update UI
-        ui->boxSavePassword->setText("Password saved");
-        ui->boxSavePassword->setChecked(false);
-        ui->boxSavePassword->setEnabled(false);
-
-        delete key;
-        delete encrypted;
-    }
 
 }
 
@@ -156,6 +96,93 @@ void MainWindow::textChanged() {
 void MainWindow::indexChanged() {
     textChanged();
     generate();
+}
+
+void MainWindow::loadServices() {
+    servicesFile = new QFile("services.txt");
+    if (servicesFile->open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QString servicesString = QString::fromUtf8(servicesFile->readAll());
+        QStringList servicesRows = servicesString.split('\n', QString::SplitBehavior::SkipEmptyParts);
+
+        foreach (QString row, servicesRows) { // Split services and usernames into map
+            QStringList buffer = row.split('/', QString::SplitBehavior::SkipEmptyParts);
+            if (buffer.length() == 1) {
+                usernameMap.insert(buffer.at(0), "n/a");
+            } else if (buffer.length() == 2) {
+                usernameMap.insert(buffer.at(0), buffer.at(1));
+            } else if (buffer.length() == 3) {
+                usernameMap.insert(buffer.at(0), buffer.at(1));
+                charsetMap.insert(buffer.at(0), buffer.at(2));
+            }
+        }
+
+        QStringList services = QStringList(usernameMap.keys());
+        for(int k = 0; k < (services.size()/2); k++) services.swap(k, services.size()-(1+k)); // Reverse list
+
+        ui->comboService->insertItems(0, services);
+        ui->comboService->setCurrentText("");
+        ui->comboService->setCurrentIndex(-1);
+        servicesFile->close();
+    }
+}
+
+void MainWindow::loadPassword() {
+    passwordFile = new QFile("password");
+    if (passwordFile->open(QIODevice::ReadOnly)) {
+        QByteArray* key = crypt_backend::getLocalKey();
+        QByteArray* passwordBytes = crypt_backend::decrypt(passwordFile->readAll(), *key);
+        QString passwordPlain = QString::fromUtf8(*passwordBytes);
+        if (passwordPlain.contains("PW:")) { // Check if password is valid
+            passwordPlain.remove(0, 3);
+            ui->linePassword->setText(passwordPlain);
+            status("Password loaded", "#ddffdd");
+        } else {
+            status("Password decryption failed", "#ffdddd");
+        }
+        passwordFile->close();
+        delete key;
+        delete passwordBytes;
+    }
+}
+
+void MainWindow::savePassword() {
+    if (ui->linePassword->text().length() < minPwLength) {
+        status("Password too short", "#ffdddd");
+        return;
+    }
+    if (passwordFile->open(QIODevice::WriteOnly)) {
+
+        // Obtain key and encrypt password
+        QByteArray* key = crypt_backend::getLocalKey();
+        QByteArray passwordBytes = ("PW:" + ui->linePassword->text()).toUtf8();
+        QByteArray* encrypted = crypt_backend::encrypt(passwordBytes, *key);
+
+        // Save to file
+        passwordFile->write(*encrypted);
+        passwordFile->close();
+
+        // Update UI
+        status("Password saved", "#ddffdd");
+
+        delete key;
+        delete encrypted;
+    } else {
+        status("Could not access file", "#ffdddd");
+    }
+}
+
+void MainWindow::deletePassword() {
+    passwordFile = new QFile("password");
+    if (passwordFile->open(QIODevice::WriteOnly)) {
+        passwordFile->remove();
+        status("Password deleted", "#ddffdd");
+    } else {
+        status("Could not access file", "#ffdddd");
+    }
+}
+
+void MainWindow::editServices() {
+    QDesktopServices::openUrl(QUrl::fromLocalFile(QFileInfo("services.txt").absoluteFilePath()));
 }
 
 void MainWindow::status(QString status, QString color) { // Color: "#rrggbb"
